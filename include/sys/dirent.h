1cbf223301d21d490bce69ad7804cc137810859a
Ultibo <admin@ultibo.org>
2023-10-02 15:28:41 +1100

    Update dirent.h to define d_type values

--- include/sys/dirent.h
+++ include/sys/dirent.h
@@ -43,6 +43,29 @@ struct dirent64
 #define HAVE_NO_D_NAMLEN	/* no struct dirent->d_namlen */
 #define HAVE_DD_LOCK  		/* have locking mechanism */
 
+/* File types for `d_type'.  */
+enum
+  {
+    DT_UNKNOWN = 0,
+# define DT_UNKNOWN	DT_UNKNOWN
+    DT_FIFO = 1,
+# define DT_FIFO	DT_FIFO
+    DT_CHR = 2,
+# define DT_CHR		DT_CHR
+    DT_DIR = 4,
+# define DT_DIR		DT_DIR
+    DT_BLK = 6,
+# define DT_BLK		DT_BLK
+    DT_REG = 8,
+# define DT_REG		DT_REG
+    DT_LNK = 10,
+# define DT_LNK		DT_LNK
+    DT_SOCK = 12,
+# define DT_SOCK	DT_SOCK
+    DT_WHT = 14
+# define DT_WHT		DT_WHT
+  };
+
 #define MAXNAMLEN 255		/* sizeof(struct dirent.d_name)-1 */
 
 typedef struct {
